324fbfc583eba823e38818988f95ca7cfe8abdc8
tyleryandt18 <tyyandt@davidson.edu>
2023-09-11 15:19:03 -0400

    Added Lincense

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,18 @@
 
 /* javascript */
+console.log("Title: Self-Care\n",
+    "Author: Mac Miller\n",
+    "Released in: 2018\n",
+    "Original: https://genius.com/Mac-miller-self-care-lyrics\n",
+    "--- License ---\n",
+    "Produced By: Dahi, Nostxlgic & I.D. Labs\n\n",
+    "Writter By: Mac Miller, JID, Dahi, Nostxlgic, E. Dan (Producer), Nice Rec,\n",
+    " Devonté Hynes, Erykah Badu & JaBor\n\n",
+    "Publishers: McNooter Publishing, Mudgeon Music, ID Labs Worldwide Sound, \n",
+    "Dahi Productions, Nostxlgic Productions, Route Way, Divine Pimp Publishing, \n",
+    "Staircase Works, BMG, Reservoir Media, Sony/ATV Sonata, Sony/ATV Tunes, \n",
+    "Sony Music Entertainment, Universal Music Group & Universal Music-MGB Songs\n\n",
+    "Performance Rights: ASCAP & BMI\n\n",
+    "Distributor: Warner Music Group\n\n",
+    "Copyright ©: Warner Records"
+)
